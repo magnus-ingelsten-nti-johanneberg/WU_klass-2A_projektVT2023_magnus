22415a83ac91dd035252a4a95c660c2e93153c23
magnus-ingelsten-nti-johanneberg <magnus.raaholtingelsten@elev.ga.ntig.se>
2023-03-06 14:59:14 +0100

    vecka 10
    utseende, anpassning, transitions etc

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const burger = document.getElementById('burger')
 const list = document.getElementById('list_pages')
 
 function toggleMenu() {
-    burger.classList.toggle('burger_toggle')
+    burger.classList.toggle('burger_active')
+    burger.classList.toggle('burger_inactive')
     list.classList.toggle('list_inactive')
     list.classList.toggle('list_active')
 }
